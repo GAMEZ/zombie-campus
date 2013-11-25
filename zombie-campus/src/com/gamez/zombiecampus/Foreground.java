7b4995782502edc74c3d1471778fe61fa50904bb
Zach <Dunlain@users.noreply.github.com>
2013-11-25 13:32:13 -0500

    Adding Foreground file for player "run surface"

---
+++ zombie-campus/src/com/gamez/zombiecampus/Foreground.java
@@ -0,0 +1,15 @@
+package com.gamez.zombiecampus;
+
+package com.gamez.zombiecampus;
+
+import java.util.Random;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+
+public class Foreground {
+  
+  public Foreground(){
+  
+  }
+}
