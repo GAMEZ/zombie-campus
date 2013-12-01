1c5115fa9485d8ac84df6137866f073549655eaa
Zach <Dunlain@users.noreply.github.com>
2013-12-01 16:01:29 -0500

    Update Controller.java
    Controller is set up with three buttons whose positions can be set with their respective setPosition methods.

--- zombie-campus/src/com/gamez/zombiecampus/Controller.java
+++ zombie-campus/src/com/gamez/zombiecampus/Controller.java
@@ -1,10 +1,33 @@
 package com.gamez.zombiecampus;
 
+import com.gamez.actordef.GAMEZPlayer;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
 public class Controller {
 	
-	public Controller(){
-		
-	}
+Button jumpB, fireB, pauseB;
 	
+    public Controller(GAMEZPlayer player, Skin uiSkin)
+    {
+	jumpB = new Button(uiSkin);
+	fireB = new Button(uiSkin);
+	pauseB = new Button(uiSkin);
+    }
 	
+    public void setJumpButtonPos(float x, float y)
+    {
+	jumpB.setPosition(x, y);
+    }
+        
+    public void setFireButtonPos(float x, float y)
+    {
+	fireB.setPosition(x, y);
+    }
+        
+    public void setPauseButtonPos(float x, float y)
+    {
+        pauseB.setPosition(x, y);
+    }
 }
